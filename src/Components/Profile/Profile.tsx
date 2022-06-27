60d4fd178b4b916076bcf1cb3d9feea1309c047f
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-27 14:09:53 +0300

    add text animation

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -12,7 +12,7 @@ const Profile = () => {
       <div className={s.content}>
       <div data-aos="fade-right" data-aos-delay="800" data-aos-duration="1200" >
         <h3>Привет! Меня зовут <span className={s.name}>Алексей Кропотин</span></h3>
-        <p>Я front-end разработчик. </p>
+        <p className="line-1 anim-typewriter">Я front-end разработчик. </p>
       </div>
       <div data-aos="fade-left" data-aos-delay="400" data-aos-duration="1200" className={s.photo}>
         <img src="https://s1.cdn.teleprogramma.pro/wp-content/uploads/2020/01/a76ebd11ecf1ab90a360b056f49b90a0.jpg" alt="profile_image"/>
