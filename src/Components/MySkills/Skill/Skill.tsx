2cec816a3ae1267537cf9cd8c41a27f1165ec9ee
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-14 12:41:08 +0300

    add style from card skill

--- src/Components/MySkills/Skill/Skill.tsx
+++ src/Components/MySkills/Skill/Skill.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import s from './Skill.module.css'
+import Tilt from "react-parallax-tilt";
 type SkillPropsType = {
   title?: string
   icon?: any
   description?: string
 }
 const Skill = (props: SkillPropsType) => {
-  console.log(props)
   return (
-    <div data-aos="fade-right" data-aos-delay="400" data-aos-duration="1200" className={s.wrapper}>
-     <div className={s.blockTitle}>
-       <p className={s.title}>{props.title}</p>
-       {props.icon}
-     </div>
-      <div className={s.descriptionBlock} data-aos="zoom-in" data-aos-delay="500"
-           data-aos-duration="1000"
-           data-aos-easing="ease-in-out">
-       <p className={s.descriptionTitle}>{props.description}</p>
-     </div>
-    </div>
+
+      <Tilt>
+        <div data-aos="fade-right" data-aos-delay="400" data-aos-duration="1200" className={s.wrapper}>
+          <div className={s.blockTitle}>
+          <p className={s.title}>{props.title}</p>
+          {props.icon}
+        </div>
+          <div className={s.descriptionBlock} data-aos="zoom-in" data-aos-delay="500"
+               data-aos-duration="1000"
+               data-aos-easing="ease-in-out">
+            <p className={s.descriptionTitle}>{props.description}</p>
+          </div>
+        </div>
+      </Tilt>
+
   );
 };
 
