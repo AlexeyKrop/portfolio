986b0ca5ba05183075069e7d58a518f1a2ade5a4
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-15 08:54:37 +0300

    add bg

--- src/Components/MyWorks/MyWorks.tsx
+++ src/Components/MyWorks/MyWorks.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './MyWorks.module.css'
 import Work from '../MyWorks/Work/Work';
-
+import socialNetworkBg from '../../Asset/social_network.png';
 const descriptionWork = {
   socialNetwork: {
     description: 'The project is made on React on class components,written in TypeScript, used Redux, Redux-Thunk as state management, React-Router-Dom, CSS native',
     source: 'https://github.com/AlexeyKrop/social-network',
-    deployment: 'https://github.com/AlexeyKrop/social-network'
+    deployment: 'https://github.com/AlexeyKrop/social-network',
+    bg: {socialNetworkBg}
   },
   todolist: {
     description:'The project was made on React on functional components, written in TypeScript,  Redux, Redux-thunk was used as state management, UI was developed using the MUI library, React-Router-Dom',
@@ -26,7 +27,7 @@ const MyWorks = () => {
           </div>
           <div className={s.block}>
             <Work title={'Social-network'} source={descriptionWork.socialNetwork.source}
-                  description={descriptionWork.socialNetwork.description}/>
+                  description={descriptionWork.socialNetwork.description} bg={descriptionWork.socialNetwork.bg}/>
             <Work title={'Todolist'} source={descriptionWork.todolist.source} deployment={descriptionWork.todolist.deployment}
                   description={descriptionWork.todolist.description}/>
             <Work title={'Card'} source={descriptionWork.socialNetwork.source}
--- src/Components/MyWorks/Work/Work.tsx
+++ src/Components/MyWorks/Work/Work.tsx
@@ -10,13 +10,19 @@ type WorkProps = {
   source?: string
   title?: string
   description?:string
+  bg?: {
+    socialNetworkBg: string
+  }
 }
 const Work = (props: WorkProps) => {
+  console.log(props.bg)
   const [scale, setScale] = useState(1.15);
   return (
     <Tilt tiltEnable={false} scale={scale} transitionSpeed={2500}>
       <div className={s.wrapper} data-aos="fade-right" data-aos-delay="400" data-aos-duration="1200">
-        <div className={s.block__title}>
+        <div className={s.block__title}  style={{
+          backgroundImage: `url(${props.bg ? props.bg.socialNetworkBg : 'https://img.tourister.ru/files/2/5/3/2/5/1/3/1/original.jpg'})`,
+        }} >
           <div className={s.btnGroup}>
             <Button href={props.source} name={'View Source'} icon={<AiFillGithub/>}/>
             <Button href={props.deployment} name={'Deployment'} icon={<IoEarth/>}/></div>
