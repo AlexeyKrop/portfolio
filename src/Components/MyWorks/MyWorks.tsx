01a9b4ca5c15823aba9ffc5430add447a9711336
EnzoK <kropotin_aleksey@mail.ru>
2022-09-07 16:02:19 +0300

    feat: add two project

--- src/Components/MyWorks/MyWorks.tsx
+++ src/Components/MyWorks/MyWorks.tsx
@@ -4,6 +4,8 @@ import Work from '../MyWorks/Work/Work';
 import socialNetworkBg from '../../Assets/social_network.png';
 import todolist from '../../Assets/todolist.png';
 import cards from '../../Assets/card.png'
+import counter from '../../Assets/counter.png'
+import tourPlan from '../../Assets/tourplan.png'
 import {GitHubView} from "../GitHubView/GitHubView";
 
 const descriptionWork = {
@@ -20,10 +22,22 @@ const descriptionWork = {
     bg: todolist
   },
   cards: {
-    description:'The project was made on React on functional components with hooks, written in TypeScript, Redux was used as state management, UI was developed using the MUI library, used Redux-thunk, React-Router-Dom, formik',
-    source: 'https://github.com/AlexeyKrop/react-todo',
-    deployment: 'https://react-todolist-by.herokuapp.com/login',
+    description:'Team project, the project was made on React on functional components with hooks, written in TypeScript, Redux was used as state management, UI was developed using the MUI library, used Redux-thunk, React-Router-Dom, formik',
+    source: 'https://github.com/DmitriyKaplan28/Friday',
+    deployment: 'https://dmitriykaplan28.github.io/Friday/#/login',
     bg: cards
+  },
+  counter: {
+    description:'simple project made with react and redux and css',
+    source: 'https://github.com/AlexeyKrop/counter.github.io',
+    deployment: 'https://alexeykrop.github.io/counter.github.io/',
+    bg: counter
+  },
+  tourPlan: {
+    description:'TourPlan simple static landing page for hotel. Written in HTML5, Sass, JQuery, PHPMiler',
+    source: 'https://github.com/AlexeyKrop/tour-plan',
+    deployment: 'https://alexeykrop.github.io/tour-plan/',
+    bg: tourPlan
   }
 }
 
@@ -40,8 +54,12 @@ const MyWorks = () => {
                   description={descriptionWork.socialNetwork.description} bg={descriptionWork.socialNetwork.bg}/>
             <Work title={'Todolist'} source={descriptionWork.todolist.source} deployment={descriptionWork.todolist.deployment}
                   description={descriptionWork.todolist.description} bg={descriptionWork.todolist.bg}/>
-            <Work title={'Card'} source={descriptionWork.cards.source}
+            <Work title={'Card'} source={descriptionWork.cards.source} deployment={descriptionWork.cards.deployment}
                   description={descriptionWork.cards.description} bg={descriptionWork.cards.bg}/>
+            <Work title={'Counter'} source={descriptionWork.counter.source} deployment={descriptionWork.counter.deployment}
+                  description={descriptionWork.counter.description} bg={descriptionWork.counter.bg}/>
+            <Work title={'Tour-plan'} source={descriptionWork.tourPlan.source} deployment={descriptionWork.tourPlan.deployment}
+                  description={descriptionWork.tourPlan.description} bg={descriptionWork.tourPlan.bg}/>
           </div>
         </div>
         <div className={s.githubView} data-aos="fade-right" data-aos-delay="400" data-aos-duration="1200">
