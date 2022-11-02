7cd26fedd1a7c82dfeabc6d76d7024dcec470061
EnzoK <kropotin_aleksey@mail.ru>
2022-11-02 12:10:41 +0300

    feat: add skill set

--- src/Components/MySkills/MySkills.tsx
+++ src/Components/MySkills/MySkills.tsx
@@ -8,6 +8,10 @@ import {SiJavascript} from "@react-icons/all-files/si/SiJavascript";
 import {DiSass} from "@react-icons/all-files/di/DiSass";
 import {BiGitBranch} from "@react-icons/all-files/bi/BiGitBranch";
 import {SiJest} from "@react-icons/all-files/si/SiJest";
+import {FaNode} from "@react-icons/all-files/fa/FaNode";
+import {DiMongodb} from "@react-icons/all-files/di/DiMongodb";
+import {DiCss3} from "@react-icons/all-files/di/DiCss3";
+import {AiFillApi} from "@react-icons/all-files/ai/AiFillApi";
 
 const MySkills = () => {
   return (
@@ -24,12 +28,14 @@ const MySkills = () => {
             <Skill icon={<SiTypescript className={s.iconSkill}/>} title={'TS'}/>
             <Skill icon={<SiJavascript className={s.iconSkill}/>} title={'JS'}/>
             <Skill icon={<SiJest className={s.iconSkill}/>} title={'Unit test'}/>
+            <Skill icon={<AiFillApi className={s.iconSkill}/>} title={'Rest API'}/>
             {/*<Skill icon={<TbBrandReactNative className={s.iconSkill}/>} title={'React Native'}/>*/}
-            {/*<Skill icon={<FaNode className={s.iconSkill}/>} title={'Node JS'}/>*/}
-            {/*<Skill icon={<AiFillHtml5 className={s.iconSkill}/>} title={'Html5'}/>*/}
-            {/*<Skill icon={<DiCss3 className={s.iconSkill}/>} title={'Css'}/>*/}
+            {/*<Skill icon={<DiAngularSimple className={s.iconSkill}/>} title={'Angular'}/>*/}
+            <Skill icon={<DiCss3 className={s.iconSkill}/>} title={'Css'}/>
             <Skill icon={<DiSass className={s.iconSkill}/>} title={'Sass'}/>
             <Skill icon={<BiGitBranch className={s.iconSkill}/>} title={'Git'}/>
+            <Skill icon={<FaNode className={s.iconSkill}/>} title={'Node JS'}/>
+            <Skill icon={<DiMongodb className={s.iconSkill}/>} title={'MongoDB'}/>
           </div>
         </div>
       </div>
